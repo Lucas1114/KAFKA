11fe150b98b5321369cf3b00db2e26070cbf5bd1
Lucas1114 <lucas.feb4@gmail.com>
2023-12-31 17:18:06 +1100

    Created
    add a consumer

---
+++ src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -0,0 +1,12 @@
+package dev.lydtech.dispatch.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DispatchService {
+
+    public void process(String payload){
+
+    }
+}
