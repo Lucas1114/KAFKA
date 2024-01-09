4d3859004bbb2419348ef625e63286680d295c2f
Lucas1114 <lucas.feb4@gmail.com>
2024-01-09 18:14:11 +1100

    33. Spring Bean Configuration

--- src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -1,9 +1,11 @@
 package dev.lydtech.dispatch.service;
 
+import dev.lydtech.dispatch.message.OrderCreated;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import dev.lydtech.dispatch.util.TestEventData;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.util.UUID.randomUUID;
 
 class DispatchServiceTest {
 
@@ -18,6 +20,7 @@ class DispatchServiceTest {
     @Test
     void process() {
 
-        service.process("payload");
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(),randomUUID().toString());
+        service.process(testEvent);
     }
 }
